cdd0af8413ced54e78d7b5134716272ef84adc11
Jaspreet Singh <dev@jas.bio>
2018-06-18 22:03:41 +0530

    Expose test Field

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,35 @@
 const TypeORM = require('typeorm');
 const TypeGQL = require('type-graphql');
 export { TypeORM, TypeGQL };
+
+import { ReturnTypeFunc, AdvancedOptions, TypeValue } from 'type-graphql/decorators/types';
+
+
+
+// const { ReturnTypeFunc, AdvancedOptions, TypeValue };
+
+// const { ReturnTypeFunc, AdvancedOptions, TypeValue } = require('type-graphql/decorators/types')
+
+export function Field(returnTypeFunc?: ReturnTypeFunc, options?: AdvancedOptions): PropertyDecorator
+{
+    options = { nullable: true, ...options }
+    return (target, propertyKey) =>
+    {
+        TypeGQL.Field(returnTypeFunc, <AdvancedOptions>options)(target, propertyKey);
+    };
+}
+
+// export function FieldOneToMany<T>(
+//     typeFunction: (type?: any) => TORM.ObjectType<T>,
+//     inverseSide: string | ((object: T) => any),
+//     options?: Relation | AdvancedOptions
+// ): PropertyDecorator
+// {
+//     options = { nullable: true, ...(options) };
+//     return (target, propertyKey) =>
+//     {
+//         TGQL.Field(typeFunction, options as AdvancedOptions)
+//             (target, propertyKey);
+//         TORM.OneToMany(typeFunction, inverseSide, options as RelationOption);
+//     };
+// }
